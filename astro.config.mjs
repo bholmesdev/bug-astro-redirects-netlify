81a357a56ce48c9b195d2cc11ac4aff34cbdf01c
bholmesdev <hey@bholmes.dev>
2022-04-19 17:08:40 -0400

    feat: update deploy to netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node(),
+  adapter: netlify(),
 });
